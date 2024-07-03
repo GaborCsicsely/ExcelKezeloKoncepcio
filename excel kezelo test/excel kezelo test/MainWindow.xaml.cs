ce5ddeeb5028827812ac8ce246008644fcbc2325
GaborCsicsely <90259975+GaborCsicsely@users.noreply.github.com>
2024-07-03 19:31:22 +0200

    Add files via upload

---
+++ excel kezelo test/excel kezelo test/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using ClosedXML.Excel;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace excel_kezelo_test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DataTable dataTable;
+        private string currentFilePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenTableButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xlsx;*.xlsm;*.xlsb;*.xls";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                currentFilePath = openFileDialog.FileName;
+                LoadExcel(currentFilePath);
+            }
+        }
+
+        private void LoadExcel(string filePath)
+        {
+            using (var workbook = new XLWorkbook(filePath))
+            {
+                var worksheet = workbook.Worksheets.First();
+                var range = worksheet.RangeUsed();
+                dataTable = new DataTable();
+
+                foreach (var headerCell in range.FirstRow().Cells())
+                {
+                    dataTable.Columns.Add(NormalizeColumnName(headerCell.GetValue<string>()));
+                }
+
+                foreach (var dataRow in range.RowsUsed().Skip(1))
+                {
+                    var newRow = dataTable.NewRow();
+                    for (int i = 0; i < dataTable.Columns.Count; i++)
+                    {
+                        newRow[i] = dataRow.Cell(i + 1).GetValue<string>();
+                    }
+                    dataTable.Rows.Add(newRow);
+                }
+
+                DataGrid.ItemsSource = dataTable.DefaultView;
+            }
+        }
+
+        private string NormalizeColumnName(string columnName)
+        {
+            string normalized = columnName.Normalize(System.Text.NormalizationForm.FormD);
+            var chars = normalized.Where(c => System.Globalization.CharUnicodeInfo.GetUnicodeCategory(c) != System.Globalization.UnicodeCategory.NonSpacingMark).ToArray();
+            normalized = new string(chars).Normalize(System.Text.NormalizationForm.FormC);
+            return normalized.Replace(' ', '_');
+        }
+
+        public void SaveToExcel()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Sheet1");
+
+                for (int i = 0; i < dataTable.Columns.Count; i++)
+                {
+                    worksheet.Cell(1, i + 1).Value = dataTable.Columns[i].ColumnName;
+                }
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataTable.Columns.Count; j++)
+                    {
+                        worksheet.Cell(i + 2, j + 1).Value = dataTable.Rows[i][j].ToString();
+                    }
+                }
+
+                workbook.SaveAs(currentFilePath);
+            }
+        }
+
+        private void SearchBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            var filterText = SearchBox.Text;
+            var dv = dataTable.DefaultView;
+            dv.RowFilter = string.Format("Column1 LIKE '%{0}%'", filterText);
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var newRow = dataTable.NewRow();
+            dataTable.Rows.Add(newRow);
+            var modifyWindow = new ModifyWindow(newRow, this);
+            modifyWindow.ShowDialog();
+            DataGrid.Items.Refresh();
+        }
+
+        private void ModifyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGrid.SelectedItem != null)
+            {
+                var selectedRow = ((DataRowView)DataGrid.SelectedItem).Row;
+                var modifyWindow = new ModifyWindow(selectedRow, this);
+                modifyWindow.ShowDialog();
+                DataGrid.Items.Refresh();
+            }
+        }
+
+        private void DeactivateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGrid.SelectedItem != null)
+            {
+                var selectedRow = ((DataRowView)DataGrid.SelectedItem).Row;
+                DataGrid.Items.Refresh();
+                SaveToExcel();
+            }
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ excel kezelo test/excel kezelo test/ModifyWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace excel_kezelo_test
+{
+    /// <summary>
+    /// Interaction logic for ModifyWindow.xaml
+    /// </summary>
+    public partial class ModifyWindow : Window
+    {
+        private DataRow dataRow;
+        private MainWindow mainWindow;
+
+        public ModifyWindow(DataRow row, MainWindow mainWindow)
+        {
+            InitializeComponent();
+            dataRow = row;
+            this.mainWindow = mainWindow;
+            CreateFields();
+        }
+
+        private void CreateFields()
+        {
+            foreach (DataColumn column in dataRow.Table.Columns)
+            {
+                StackPanel stackPanel = new StackPanel { Orientation = Orientation.Horizontal };
+                TextBlock textBlock = new TextBlock
+                {
+                    Text = column.ColumnName,
+                    Width = 100,
+                    Margin = new Thickness(5)
+                };
+                TextBox textBox = new TextBox
+                {
+                    Name = column.ColumnName,
+                    Text = dataRow[column.ColumnName].ToString(),
+                    Width = 200,
+                    Margin = new Thickness(5)
+                };
+
+                stackPanel.Children.Add(textBlock);
+                stackPanel.Children.Add(textBox);
+                FieldsPanel.Children.Add(stackPanel);
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var child in FieldsPanel.Children)
+            {
+                if (child is StackPanel stackPanel)
+                {
+                    foreach (var innerChild in stackPanel.Children)
+                    {
+                        if (innerChild is TextBox textBox)
+                        {
+                            dataRow[textBox.Name] = textBox.Text;
+                        }
+                    }
+                }
+            }
+            mainWindow.SaveToExcel();
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
